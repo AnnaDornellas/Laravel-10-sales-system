11697f708da5e28f656d551d4b45670de3dbd68e
annadornellas <anna.ds00123@gmail.com>
2025-07-04 12:25:33 -0300

    up

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
+
 
 class ProdutosController extends Controller
 {
-    public function index(){
-        return 'produtos';
+    protected $produto;
+
+    public function __construct(Produto $produto){
+
+        $this->produto = $produto;
+    }
+    public function index(Request $request){
+        $findProduto = Produto::all();
+        
+        return  view('pages.produtos.paginacao' , compact('findProduto'));
     }
 }
